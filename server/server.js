66c07b7bf06cd40f350d8f94130f0dd98bd31bbc
Kassidy Ralph <89705830+kassidyr@users.noreply.github.com>
2022-02-16 17:18:10 -0500

    added authentication

--- server/server.js
+++ server/server.js
@@ -1,19 +1,21 @@
-const express = require('express');
+const express = require("express");
 // import ApolloServer
-const { ApolloServer } = require('apollo-server-express');
+const { ApolloServer } = require("apollo-server-express");
 
 // import our typeDefs and resolvers
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 const startServer = async () => {
   // create a new Apollo server and pass in our schema data
-  const server = new ApolloServer({ 
-    typeDefs, 
-    resolvers
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
   });
 
   // Start the Apollo server
@@ -32,7 +34,7 @@ startServer();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
   });
