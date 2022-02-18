ff0bed1b05ab544ab84f73985239eebfc8df0309
Kassidy Ralph <89705830+kassidyr@users.noreply.github.com>
2022-02-18 17:34:33 -0500

    added authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import './App.css';
 
 import Header from './components/Header';
@@ -17,9 +18,18 @@ import Signup from './pages/Signup';
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 //Use ApolloClient() to instantiate the Apollo Client instance and create the connection to the API endpoint
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../utils/mutations";
+import Auth from '../utils/auth';
 
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: "", password: "" });
+  const [login, { error }] = useMutation(LOGIN_USER);
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -17,11 +21,15 @@ const Login = (props) => {
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
-    // clear form values
-    setFormState({
-      email: "",
-      password: "",
-    });
+    try {
+      const { data } = await login({
+        variables: { ...formState }
+      });
+    
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
   };
 
   return (
@@ -53,6 +61,7 @@ const Login = (props) => {
                 Submit
               </button>
             </form>
+            {error && <div>Login failed</div>}
           </div>
         </div>
       </div>
