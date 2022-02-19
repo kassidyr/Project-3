fd80ded60d214c18508cdc4d07d8b2919552d3de
Kassidy Ralph <89705830+kassidyr@users.noreply.github.com>
2022-02-19 11:27:24 -0500

    update logout function

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,7 +1,14 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import Auth from "../../utils/auth";
 
 const Header = () => {
+  const logout = (event) => {
+    //overrides the <a> element's default of having the browser load a different resource
+    event.preventDefault();
+    //removes token from localStorage and refreshes the application to take the user back to the login page
+    Auth.logout();
+  };
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
@@ -10,8 +17,19 @@ const Header = () => {
         </Link>
 
         <nav className="text-center">
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Signup</Link>
+          {Auth.loggedIn() ? (
+            <>
+              <Link to="/profile">Me</Link>
+              <a href="/" onClick={logout}>
+                Logout
+              </a>
+            </>
+          ) : (
+            <>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Signup</Link>
+            </>
+          )}
         </nav>
       </div>
     </header>
