41b22feafa26dd6b3b0a2a184e37b8e58c51e545
Kassidy Ralph <89705830+kassidyr@users.noreply.github.com>
2022-02-20 21:35:32 -0500

    updated header and footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,7 +3,13 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="w-100 mt-auto bg-secondary p-4">
-      <div className="container">&copy;2022 by Spacebook</div>
+      <br></br>
+      <div className="container">
+        
+        &copy;2022 by spacebook
+        
+      </div>
+      <br></br>
     </footer>
   );
 };
